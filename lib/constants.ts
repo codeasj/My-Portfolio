6e4bb720da4b0e4f96f85070601d642dc66fef50
Anuj <codeasj@outlook.com>
2026-04-06 19:00:55 +0530

    constsnts

--- lib/constants.ts
+++ lib/constants.ts
@@ -21,5 +21,5 @@ export const SOCIAL_LINKS = {
   github: "https://github.com/codeasj",
   linkedin: "https://www.linkedin.com/in/anujsrivastava0/",
   email: "anujs1369@gmail.com",
-  resume: "https://drive.google.com/file/d/1N_MkmnA23r65v0D_joyw-uONDQ_0OfDY/view?usp=sharing",
+  resume: "https://drive.google.com/file/d/1oKYgT8A2QAa9FV3SWwhQWeN1FBcPyZ_f/view?usp=sharing",
 } as const;
